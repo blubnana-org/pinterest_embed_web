c26644347ab510a208a1fc6aba8e3ee61dbaa946
khomotsozwane <blubnana@gmail.com>
2025-08-14 21:03:58 +0200

    update plugin code

--- pinterest_embed_plugin/lib/pinterest_embed_plugin.dart
+++ pinterest_embed_plugin/lib/pinterest_embed_plugin.dart
@@ -1,8 +1,23 @@
+import 'package:flutter/widgets.dart';
 
 import 'pinterest_embed_plugin_platform_interface.dart';
 
-class PinterestEmbedPlugin {
-  Future<String?> getPlatformVersion() {
-    return PinterestEmbedPluginPlatform.instance.getPlatformVersion();
+class PinterestEmbedPlugin extends StatelessWidget {
+  final String boardUrl;
+  final int boardWidth; 
+  final int scaleHeight;
+  final int scaleWidth;
+
+  const PinterestEmbedPlugin({
+    super.key,
+    required this.boardUrl,
+    this.boardWidth = 1920,
+    this.scaleHeight = 1080,
+    this.scaleWidth = 200,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
